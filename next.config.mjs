21847755e624399878cfebb93422547b63977198
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-05-02 19:57:32 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+
+    env:{
+        //RazorPay keys
+        RAZORPAY_KEY:'rzp_test_VPWQhQ0jb6Lbez',
+        RAZORPAY_SECRET:'qZJKFCNztPj6zLwGXoJ768le',
+    },
+
+
+
+
+
+};
+
+export default nextConfig;
